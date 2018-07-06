57dd9d44c96e0286443e69d1925343b59cab83bc
Peter Maguire <pjmaguire3@gmail.com>
2018-07-06 14:56:25 -0700

    Add files via upload

---
+++ DNAnexus_Mass_Downloader.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#author=Peter_Maguire
+#contact=pmaguire@stanford.edu
+#Requirements=dx-toolkit
+
+projects=${@}
+
+for project in $projects; do
+	mkdir $project
+	cd $project
+	dx select $project
+
+	dx download -r "/"
+	cd ..
+done
+
+echo "Finished!"
